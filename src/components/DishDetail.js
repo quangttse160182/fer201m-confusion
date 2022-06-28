d6b4571ea2715ef6f4fe1bee84f27e1bf3f703f9
Thanh Quang <quangttse160182@fpt.edu.vn>
2022-06-28 08:37:53 +0700

    Functional Components

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -1,46 +1,45 @@
-import { Component } from 'react';
 import { CardBody, CardTitle, CardText, CardImg, Card } from 'reactstrap';
 
-export class DishDetail extends Component {
-  constructor(props) {
-    super(props);
-  }
+const RenderDish = ({ dish }) => {
+  if (dish)
+    return (
+      <Card>
+        <CardImg top src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
+  return <div></div>;
+};
 
-  render() {
-    if (!!this.props.dish) {
-      const comment = this.props.dish.comments.map((item) => {
-        return (
-          <div key={item.id}>
-            <p>{item.comment}</p>
-            <p>
-              {item.author} {item.date}
-            </p>
-          </div>
-        );
-      });
-      const dish = this.props.dish;
+const RenderComments = ({ comments }) => {
+  comments = comments?.map((item) => {
+    return (
+      <div key={item.id}>
+        <p>{item.comment}</p>
+        <p>
+          {item.author} {item.date}
+        </p>
+      </div>
+    );
+  });
+  return comments;
+};
 
-      return (
-        <div className='container'>
-          <div className='row'>
-            <div className='col-12 col-sm-5'>
-              <Card>
-                <CardImg top src={dish.image} alt={dish.name} />
-                <CardBody>
-                  <CardTitle>{dish.name}</CardTitle>
-                  <CardText>{dish.description}</CardText>
-                </CardBody>
-              </Card>
-            </div>
-            <div className='col-12 col-sm-7'>
-              <h2>Comments</h2>
-              {comment}
-            </div>
-          </div>
+export const DishDetail = ({ dish }) => {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-12 col-sm-5'>
+          <RenderDish dish={dish} />
         </div>
-      );
-    } else {
-      return <div></div>;
-    }
-  }
-}
+        <div className='col-12 col-sm-7'>
+          <h2>Comments</h2>
+          <RenderComments comments={dish?.comments} />
+        </div>
+      </div>
+    </div>
+  );
+};
