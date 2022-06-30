b4aca3065c278c70a458db79d10c543fd4b8cdf4
Thanh Quang <quangttse160182@fpt.edu.vn>
2022-06-30 07:33:31 +0700

    Single Page Applications Part 2

--- src/components/DishDetail.js
+++ src/components/DishDetail.js
@@ -1,15 +1,27 @@
-import { CardBody, CardTitle, CardText, CardImg, Card } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Component } from 'react';
+import {
+  CardBody,
+  CardTitle,
+  CardText,
+  CardImg,
+  Card,
+  Breadcrumb,
+  BreadcrumbItem,
+} from 'reactstrap';
 
 const RenderDish = ({ dish }) => {
   if (dish)
     return (
-      <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
-        <CardBody>
-          <CardTitle>{dish.name}</CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
+      <div className='col-12 col-sm-5'>
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
     );
 };
 
@@ -27,24 +39,45 @@ const RenderComments = ({ comments }) => {
 
   if (comments)
     return (
-      <>
+      <div className='col-12 col-sm-7'>
         <h2>Comments</h2>
         <div>{comments}</div>
-      </>
+      </div>
     );
 };
 
-export const DishDetail = ({ dish }) => {
-  return (
-    <div className='container'>
-      <div className='row'>
-        <div className='col-12 col-sm-5'>
-          <RenderDish dish={dish} />
+export class DishDetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const dish = this.props?.dish;
+    const comments = this.props?.comments;
+    
+    return (
+      <div className='container'>
+        <div className='row'>
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to='/menu'>Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className='col-12'>
+            <h3>{dish.name}</h3>
+            <hr />
+          </div>
         </div>
-        <div className='col-12 col-sm-7'>
-          <RenderComments comments={dish?.comments} />
+        <div className='row'>
+          <div className='col-12 col-md-5 m-1'>
+            <RenderDish dish={dish} />
+          </div>
+          <div className='col-12 col-md-5 m-1'>
+            <RenderComments comments={comments} />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
--- src/components/Main.js
+++ src/components/Main.js
@@ -10,6 +10,7 @@ import { Header } from './Header';
 import { Footer } from './Footer';
 import { Home } from './Home';
 import { Contact } from './Contact';
+import { DishDetail } from './DishDetail';
 
 export class Main extends Component {
   constructor(props) {
@@ -38,6 +39,22 @@ export class Main extends Component {
         />
       );
     };
+
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
@@ -49,6 +66,7 @@ export class Main extends Component {
             component={() => <Menu dishes={this.state.dishes} />}
           />
           <Route exact path='/contactus' component={Contact} />
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Redirect to='/home' />
         </Switch>
         <Footer />
